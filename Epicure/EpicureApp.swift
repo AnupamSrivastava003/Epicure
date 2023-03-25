f4cfaace9d2682aa18dc7238e8da24890a481686
anupam <anupam4944@gmail.com>
2023-03-25 20:20:22 +0530

    Initial Commit

---
+++ Epicure/EpicureApp.swift
@@ -0,0 +1,17 @@
+//
+//  EpicureApp.swift
+//  Epicure
+//
+//  Created by SKUBI on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct EpicureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
